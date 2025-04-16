d89cec38c0c63b8fa6bc66ae74c90f5341731db7
karljenn1994 <karljenn1994@gmail.com>
2025-04-16 11:31:21 -0300

    Create puzzle.js

---
+++ puzzle.js
@@ -0,0 +1,34 @@
+[
+  {
+    "word": "MEDICAL",
+    "clue": "A procedure a doctor may perform"
+  },
+  {
+    "word": "DECIMAL",
+    "clue": "A system used by Dewey"
+  },
+  {
+    "word": "CLAIMED",
+    "clue": "A prospector already staked a gold find"
+  },
+  {
+    "word": "MILD",
+    "clue": "Weather that's neither hot nor cold"
+  },
+  {
+    "word": "LID",
+    "clue": "A cover or a hat"
+  },
+  {
+    "word": "IDLE",
+    "clue": "Sitting around doing nothing"
+  },
+  {
+    "word": "DELIST",
+    "clue": "Remove from the market"
+  },
+  {
+    "word": "TIDES",
+    "clue": "Ebbs and flows"
+  }
+]
